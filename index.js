963e9a14ff97be3f454cb5e3c326ee5a66b10f36
shubham parte <shubham parte>
2018-05-23 13:12:22 +0530

    Add mailing Feature

--- index.js
+++ index.js
@@ -7,24 +7,28 @@
      passportLocalMongoose = require("passport-local-mongoose"),
      User                  = require("./models/user"),
      Blog                  = require("./models/blogs"),
+     nodemailer            = require("nodemailer"),
      app                   = express();
 
+        //Blog and Authentication Routes
         var blogRoutes     = require("./routes/blog"),
             indexRoutes    = require("./routes/index");             
             
 //Connect To Database
-// mongoose.connect("mongodb://localhost/blog_demo");
-mongoose.connect("mongodb://siddhu:kyliejenner@ds231090.mlab.com:31090/blog");
+mongoose.connect("mongodb://localhost/blog_demo");
+// mongoose.connect("mongodb://siddhu:kyliejenner@ds231090.mlab.com:31090/blog");
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 app.use(bodyParser.urlencoded({extended: true})); 
+
 app.use(require("express-session")({
 	secret: "I love kylie Jenner",
 	resave: false,
 	saveUninitialized: false
 }));
+
 app.use(passport.initialize());
 app.use(passport.session()); 
 passport.use(new localStrategy(User.authenticate()));
@@ -36,6 +40,21 @@ app.use(function(req, res, next){
   next();
 });
 
+/*
+    Here we are configuring our SMTP Server details.
+    STMP is mail server which is responsible for sending and recieving email.
+*/
+// var smtpTransport = nodemailer.createTransport({
+//     service: "gmail",
+//     host: "smtp.gmail.com",
+//     auth: {
+//         user: "gouda.siddhu@gmail.com",
+//         pass: "gangster111"
+//     }
+// });
+/*------------------SMTP Over-----------------------------*/
+
+//Blog and auth Routes
 app.use(blogRoutes);
 app.use(indexRoutes);
 
--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,54 @@
-var express  = require("express"),
-    User     = require("../models/user"),
-    passport = require("passport"),
-    router   = express.Router();
+var express    = require("express"),
+    User       = require("../models/user"),
+    passport   = require("passport"),
+    router     = express.Router(),
+    nodeMailer = require("nodemailer");
+
+   
 
 router.get("/register", function(req, res){
    res.render("register");
 });
 
 router.post("/register", function(req, res){
-	var newUser = new User({username: req.body.username});
+
+  var email = req.body.email;
+	var newUser     = new User({username: req.body.username, email: email });
 	User.register(newUser, req.body.password, function(err, user){
         if(err){
         	console.log("Error Something Went Wrong");
         	res.redirect("/register");
         }
         passport.authenticate("local")(req, res, function(){
-        	res.redirect("/blogs");
+          console.log("Authentication started");
+          let transporter = nodeMailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true,
+            auth: {
+                user: 'purplewebcreations@gmail.com',
+                pass: '59910113'
+            }
         });
+        let mailOptions = {
+            from: '"Siddhu Gouda" <purplewebcreations@gmail.com>', // sender address
+            to: email, // list of receivers
+            //subject: req.body.subject, // Subject line
+            //text: req.body.body, // plain text body
+            html: '<b>NodeJS Email Tutorial</b>' // html body
+        };
+
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                return console.log(error);
+            }
+            console.log('Message  sent:', email);
+                res.redirect('/blogs');
+            });    
+        });
+       
 	});
+
 });
 
 router.get("/login", function(req, res){
